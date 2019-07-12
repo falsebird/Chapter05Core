013e54a61d001e50a26aecd5588acf96620ac3ad
jiashihai <874105638@qq.com>
2019-07-12 15:20:45 +0800

    实现了图片的整体混合和[F感兴趣区域的混合

--- firstStgeRemixImg.cpp
+++ firstStgeRemixImg.cpp
@@ -27,8 +27,54 @@ bool ROI_Addimage() {
 	
 
 }
+
+//-------------------------线性混合操作---------------------
+bool LinerBlening() {
+	double alphaValue = 0.5;
+	double betaValue;
+	Mat srcImage2 = imread("img/mogu.jpg");
+	Mat srcImage3 = imread("img/rain.jpg");
+	Mat dstImaga;
+	if (srcImage2.empty()) {
+		cout << "读取srcImage2错误~！" << endl;
+		return false;
+	}
+	if (srcImage3.empty()) {
+		cout << "读取srcImage3错误~！" << endl;
+		return false;
+	}
+
+	betaValue = (1.0 - alphaValue);
+	addWeighted(srcImage2, alphaValue, srcImage3, betaValue, 0.0, dstImaga);
+	namedWindow("<2>线性混合示例窗口【原图】", 1);
+	imshow("<2>线性混合示例窗口【原图】", srcImage2);
+	namedWindow("<3>线性混合示例窗口【效果图】", 1);
+	imshow("<3>线性混合示例窗口【效果图】", dstImaga);
+
+}
+//
+bool ROI_LinearBlendig() {
+	Mat srcImage4 = imread("img/dota_pa.jpg");
+	Mat logoImage1 = imread("img/dota_logo.jpg");
+	if (srcImage4.empty()) {
+		cout << "读取srcImage错误~！" << endl;
+		return false;
+	}
+	if (logoImage1.empty()) {
+		cout << "读取logoImage错误~！" << endl;
+		return false;
+	}
+
+	//两种添加感兴趣区域的方式
+	Mat imageROI = srcImage4(Rect(150, 200, logoImage1.cols, logoImage1.rows));
+	addWeighted(imageROI, 0.5, logoImage1, 0.3, 0., imageROI);
+	namedWindow("<4>线性混合示例窗口【效果图】", 1);
+	imshow("<4>线性混合示例窗口【效果图】", srcImage4);
+}
 int main() {
-	ROI_Addimage();
+	//ROI_Addimage();
+	//LinerBlening();
+	ROI_LinearBlendig();
 	waitKey(0);
 	return 0;
 }
