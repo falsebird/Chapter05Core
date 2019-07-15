4d99fddb23fbf44dbc2a9a34f3629523be87e469
jiashihai <874105638@qq.com>
2019-07-15 19:54:28 +0800

    完成调整亮度和对比度的程序问题已解决[F

--- AdjustBrightAndContrast.cpp
+++ AdjustBrightAndContrast.cpp
@@ -12,17 +12,18 @@ int g_nBrightValue;//亮度值
 Mat g_srcImage, g_dstImage;
 
 void ContrastAndBright(int, void*) {
-	namedWindow(WINDOS_NAME1, 1);
+
 	for (int y = 0; y < g_srcImage.rows; y++) {
-		for (int x = 0; y < g_srcImage.cols; x++) {
+		for (int x = 0; x < g_srcImage.cols; x++) {
 			for (int c = 0; c < 3; c++)
 			{
-				g_dstImage.at<Vec3b>(x, y)[c] = 
-					saturate_cast<uchar>((g_nContrastValue * 0.01) * (g_dstImage.at<Vec3b>(x, y)[c]) + g_nBrightValue);
+				//应该先写行号，再写列号
+				g_dstImage.at<Vec3b>(y, x)[c] = 
+					saturate_cast<uchar>((g_nContrastValue * 0.01) * (g_srcImage.at<Vec3b>(y, x)[c]) + g_nBrightValue);
 			}
-		}
+		} 
 	}
-	imshow(WINDOS_NAME1, g_srcImage);
+
 	imshow(WINDOS_NAME2, g_dstImage);
 }
 
@@ -30,12 +31,15 @@ void ContrastAndBright(int, void*) {
 int main() {
 
 	g_srcImage = imread("1.jpg");
-	imshow(WINDOS_NAME1, g_srcImage);
-	waitKey(0);
+	g_dstImage = imread("1.jpg");
+	//imshow(WINDOS_NAME1, g_srcImage);
+	//waitKey(0);
 	if(g_srcImage.empty()) {
 		cout << "读取图片错误！ " << endl;
 		return false;
 	}
+	namedWindow(WINDOS_NAME1, 1);
+	imshow(WINDOS_NAME1, g_srcImage);
 	namedWindow(WINDOS_NAME2, 1);
 	createTrackbar("对比度：", WINDOS_NAME2, &g_nContrastValue, 300, ContrastAndBright);
 	createTrackbar("亮  度：", WINDOS_NAME2, &g_nBrightValue, 200, ContrastAndBright);
